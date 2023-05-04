36d8e4e3eeba4b792b758b0f613c9defcab4a14c
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-05-03 21:37:20 -0300

    update ex014

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -9,12 +9,15 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         //bom dia
         img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#DAC7A2'
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
         img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#7E939E'
     } else {
         //boa noite
         img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#271F28'
     }
 }
 
