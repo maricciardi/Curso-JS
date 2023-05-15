cdec1d83e61c17787a028b4b3a7e68afdcbc95c0
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-05-15 12:31:41 -0300

    update aula013

---
+++ aula013/ambiente.js
@@ -0,0 +1,17 @@
+/* var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c<=10) */
+
+console.log('Vai começar...')
+for (var c = 1;c<=10;c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim.')
