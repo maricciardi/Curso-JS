91bdd01baf7ff77b6c1c4b2045aeb4424d2563cb
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-05-24 20:04:37 -0300

    update aula015

--- aula015/vetornatela.js
+++ aula015/vetornatela.js
@@ -1,5 +1,12 @@
 let valores = [4, 7, 3, 8, 1, 2, 9]
 
-for (let pos = 0; pos < valores.length; pos++) {
+/*for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+
+let p = valores.indexOf(8)
+console.log(`O valor 8 está na posição${p}`)
