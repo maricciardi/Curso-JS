f935598107b383ce327296718715aa8753d17ce4
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-05-24 19:48:33 -0300

    create aula015

---
+++ aula015/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 4]
+num[3] = 6
+num.push(7)
+let i = num.length
+num.sort()
+
+console.log(`Nosso vetor é ${num}`)
+console.log(`Nosso vetor tem ${i} elementos`)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [4, 7, 3, 8, 1, 2, 9]
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
