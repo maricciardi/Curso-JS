06e6f40b3ce358d766ad2f8875aa4c23b5a1962d
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-05-30 16:13:47 -0300

    update praticando

---
+++ praticando/01-listajs/script.js
@@ -0,0 +1,16 @@
+var lista = ['Laranja', 'Maça', 'Banana', 'Pera', 'Limão', 'Abacaxi']
+
+var listaUl = document.createElement('ul')
+
+var body = document.getElementsByTagName('body')
+
+body[0].appendChild(listaUl);
+
+var listaNoBody = document.getElementsByTagName('ul');
+
+for (var i = 0; i < lista.length; i++) {
+    var liFor = document.createElement('li');
+    var textoLi = document.createTextNode(lista[i]);
+    liFor.appendChild(textoLi);
+    listaNoBody[0].appendChild(liFor);
+}
