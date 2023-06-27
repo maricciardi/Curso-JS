2ee8c7f46bcc3471da14e84a67a0f4f12eb477a3
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-06-27 19:18:04 -0300

    update poo

--- poo/modificador.js
+++ poo/modificador.js
@@ -1,31 +1,36 @@
 function fcapitalizar(colecao, attr) {
     if (colecao, attr > 0){
-        return typeof colecao[0] == 'object' && attr?
-            colecao.map(item => item[attr] = item[attr].charAt(o).toUpperCase()
-            colecao.map(item => item = item.charAt(0).toUpperCase() + )
-    }
+        if (typeof colecao[0] == 'object' && attr) {
+            colecao.map(item => item[attr] = item[attr].charAt(0).toUpperCase() + item[attr].slice(1));
+        } else {
+            colecao.map(item => item = item.charAt(0).toUpperCase() + item.slice(1));
+        }
+        return colecao;
+    }   
 }
 
-function fcaixaAlta(vetor) {
+function fcaixaAlta(colecao, attr) {
     if (colecao.length > 0){
-        return typeof colecao(0) == 'object' && attr?
-            colecao.map(item => item[attr] = item[attr].toUpperCase())
-            colecao.map(item => item = item.toUpperCase())
+        if (typeof colecao[0] == 'object' && attr){
+            colecao.map(item => item[attr] = item[attr].toUpperCase());
+        } else {
+            colecao.map(item => item = item.toUpperCase());
+        }
     }
 }
 
 
-function fordenar(vetor) {
-    return attr?
+function fordenar(colecao, attr) {
+    return attr ?
         colecao.sort(function(a,b){
             return typeof a[attr] == 'number' ?
                 a[attr] - b[attr] :
                 a[attr].localeCompare(b[attr])
-        });
+        }):
         colecao.sort(function(a,b){
             return typeof a == 'number' ?
-            a - b :
-            a.localeCompare(b)
+                a - b :
+                a.localeCompare(b)
         });
     
 }
