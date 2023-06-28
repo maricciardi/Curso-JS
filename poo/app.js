acc77676fbd71353350d22ab868166971f06d467
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-06-27 21:13:29 -0300

    update poo

--- poo/app.js
+++ poo/app.js
@@ -1,5 +1,24 @@
 import modificador from './modificador.js';
 import ingredientes from './ingredientes.js';
 
+let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
 
-//modificador.capitalizar(ingredientes);
+let containerIngredientes = document.getElementById('container-ingredientes');
+
+for(let ingrediente of ingredientesOrdenados){
+    let textoHTML = `
+        <div class="ingrediente">
+            <img src="img/${ingrediente.img}" >
+            <p class="nome-ingrediente">${ingrediente.nome}</p>
+        </div>
+    `;
+    /**
+    <div class="ingrediente">
+        <img src="img/abobora.png">
+        <p class="nome-ingrediente">Abóbora</p>
+    </div>
+    */
+
+    containerIngredientes.innerHTML += textoHTML;
+}
+
